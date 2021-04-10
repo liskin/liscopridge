43e28aaa5e60d39c9884b0880f3f600c38747c2e
Tomas Janousek <tomi@nomi.cz>
2021-04-10 21:10:01 +0100

    Logging

--- src/liscopridge/server.py
+++ src/liscopridge/server.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 import bottle  # type: ignore [import]
 import pkg_resources
@@ -12,6 +13,7 @@ def parse_args() -> argparse.Namespace:
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
+    parser.add_argument('-v', '--verbose', action='count', help='Logging verbosity', default=0)
     parser.add_argument('-d', '--debug', dest='debug', help='Bottle debug mode', action='store_true')
     parser.add_argument('-p', '--port', dest='port', help='HTTP server port', default=12345)
     parser.add_argument('--host', dest='host', help='HTTP server host', default="127.0.0.1")
@@ -19,8 +21,19 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def setup_logging(args: argparse.Namespace) -> None:
+    level = (
+        logging.DEBUG if args.verbose >= 2 else
+        logging.INFO if args.verbose >= 1 else
+        logging.WARNING
+    )
+    logging.basicConfig(level=level)
+    logging.getLogger('waitress').setLevel(level)
+
+
 def main() -> None:
     args = parse_args()
+    setup_logging(args)
 
     bottle.TEMPLATE_PATH = [pkg_resources.resource_filename(__package__, "templates")]
     app.run(
