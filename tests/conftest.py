67bc0b64772412fd34b209a933cb1d5d6a4c3af3
Tomas Janousek <tomi@nomi.cz>
2021-04-11 20:13:24 +0100

    tests: Silence PytestCollectionWarning for webtest.TestApp

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
+from webtest import TestApp  # type: ignore [import]
+
 import liscopridge.cache
 
 
 def pytest_configure(config):
     liscopridge.cache._pytest = True
+
+    # https://github.com/Pylons/webtest/pull/227
+    TestApp.__test__ = False
