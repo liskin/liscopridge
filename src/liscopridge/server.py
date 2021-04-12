3c6dc1d2daaa1ddee007158d2372e3735df52325
Tomas Janousek <tomi@nomi.cz>
2021-04-12 14:06:03 +0100

    server: Use click instead of argparse

--- src/liscopridge/server.py
+++ src/liscopridge/server.py
@@ -1,39 +1,25 @@
-import argparse
 import logging
+
+import click
 
 from .app.root import app
 
 
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description="liskin's collection of protocol bridges",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-
-    parser.add_argument('-v', '--verbose', action='count', help='Logging verbosity', default=0)
-    parser.add_argument('-d', '--debug', dest='debug', help='Bottle debug mode', action='store_true')
-    parser.add_argument('-p', '--port', dest='port', help='HTTP server port', default=12345)
-    parser.add_argument('--host', dest='host', help='HTTP server host', default="127.0.0.1")
-
-    return parser.parse_args()
-
-
-def setup_logging(args: argparse.Namespace) -> None:
+def setup_logging(verbose: int) -> None:
     level = (
-        logging.DEBUG if args.verbose >= 2 else
-        logging.INFO if args.verbose >= 1 else
+        logging.DEBUG if verbose >= 2 else
+        logging.INFO if verbose >= 1 else
         logging.WARNING
     )
     logging.basicConfig(level=level)
 
 
-def main() -> None:
-    args = parse_args()
-    setup_logging(args)
-
-    app.run(
-        server='waitress',
-        host=args.host,
-        port=args.port,
-        debug=args.debug,
-    )
+@click.command()
+@click.option('-v', '--verbose', count=True, help='Logging verbosity')
+@click.option('-d', '--debug', is_flag=True, help='Bottle debug mode')
+@click.option('-p', '--port', help='HTTP server port', default=12345, show_default=True)
+@click.option('--host', help='HTTP server host', default="127.0.0.1", show_default=True)
+def main(host, port, verbose, debug):
+    """liskin's collection of protocol bridges"""
+    setup_logging(verbose)
+    app.run(server='waitress', host=host, port=port, debug=debug)
