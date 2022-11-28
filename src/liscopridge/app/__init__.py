dc3cf562965da6214fa98bda89eee0adf2f8eb4c
Tomas Janousek <tomi@nomi.cz>
2022-11-28 15:14:08 +0000

    Avoid runtime setuptools dependency
    Using `pkg_resources` meant that setuptools had to be available during
    runtime, but this dependency was undeclared. Switch to
    importlib.metadata and importlib.resources.

--- src/liscopridge/app/__init__.py
+++ src/liscopridge/app/__init__.py
@@ -1,4 +1,13 @@
-import bottle  # type: ignore [import]
-from pkg_resources import resource_filename
+from pathlib import Path
+import sys
 
-bottle.TEMPLATE_PATH = [resource_filename(__package__, "")]
+import bottle  # type: ignore [import]
+
+if sys.version_info >= (3, 9):
+    import importlib.resources as importlib_resources  # isort: skip
+else:
+    import importlib_resources  # isort: skip
+
+_template_path = importlib_resources.files(__package__)
+assert isinstance(_template_path, Path)
+bottle.TEMPLATE_PATH = [str(_template_path)]
